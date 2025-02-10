c919ed70db0ecb579b1fa42d8d43da91abfa1214
Enduxion <endux.pg@gmail.com>
2025-02-10 19:44:47 +0545

    target="_blank"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             </h1>
             <div className="text-lg mt-4 flex flex-col gap-1 items-center" >
                 <div>
-                    Welcome to <Link className="font-dancing text-xl underline text-highlight" to="/">Lilsy</Link>, a crochet business founded by <Link className="font-dancing text-xl underline text-highlight" to="https://www.instagram.com/niissshhhaa/">Nisha Thapa</Link>
+                    Welcome to <Link className="font-dancing text-xl underline text-highlight" to="/" target="_blank">Lilsy</Link>, a crochet business founded by <Link className="font-dancing text-xl underline text-highlight" to="https://www.instagram.com/niissshhhaa/" target="_blank">Nisha Thapa</Link>
                 </div>
                 <div>
                     Explore our collection of handmade crochet pieces. A perfect gift or personal keepsake.
